6579da008f38c27b8935742ebecdacd243b0e0fb
Antoine Beaupré <anarcat@debian.org>
2017-11-12 17:47:34 -0500

    add missing xlib dependency

--- setup.py
+++ setup.py
@@ -13,8 +13,6 @@ else:
 with open(os.path.join(os.path.dirname(__file__), req_file)) as f:
     requires = list(f.readlines())
 
-print '"%s"' % requires
-
 from setuptools import setup
 
 setup(name="selfspy",
